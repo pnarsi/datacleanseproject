d6c74a95a06116e3081ae40f09cbc6c3b3d97866
pnarsi <pnarsey@gmail.com>
2014-09-21 10:03:06 +1000

    Completed Assignmented

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 datacleanseproject
 ==================
+
+
+### Description  
+
+This README file describes the run-analysis.R which contains code to convert
+the original raw dataset  into a tidy independant dataset that provides the mean
+of each variable being measured for each activity and for each subject.
+
+The original dataset is available from: 
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+and is described at: 
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+### Analysis File Requirements
+
+The ANALYSIS FILE assumes that the source files for the data have been extracted
+into the working directory and that the README.TXT file of the source data is available at
+`./UCI HAR Dataset/README.txt`
+
+Furthermore the following R packages need to be installed:
+
+* `dplyr`
+* `tidyr`
+
+
+### Opening the Output File
+
+`read.table(*<filelocation>*, header=T, quote="\"")`
+
+
+### ANALYSIS FILE Description  
+
+At a high level, the analysis file performs the following actions in order
+
+1. Read in the source data files for both the training and test datasets. Each 
+dataset includes a file that has the measurements, another file describing 
+the activity the measurement is recording and third file that details the 
+subject performing the action.
+
+2. The training and test data are merged into one.
+
+3. The standard deviation and mean columns of the data are extracted and the 
+rest of the data is discarded. It is assumed that only measurement variables 
+(aka features) that have `sd()` and `mean()` in the name are of interest as these 
+have been called out explicitly as the *Standard Deviation* and the *Mean Value* 
+in the feature_info.txt of the original 
+dataset.
+
+4. The data is then augmented with the details of the activity being recorded. 
+The original dataset uses factors with 6 values to record the activity being 
+measured. The analyis converts the factor to a descriptive label before attaching
+it to the dataset.
+
+5. In the next step the subject details are added to the measurement as an 
+additional column.
+
+6. Minor cleanup is done with the variable names to make it more readable. 
+Specifically all `'()'` are removed from the name and all `'-'` are converted to `'_'`
+
+7. The next step is to convert the short, wide dataset to a long, narrow one. As
+per Hadley Wickhan data paper[1] *tidy data* is one where each variable forms a 
+column and each observation forms a row. One can argue that the various 
+'variables' in the original dataset are in reality part of a set of features that
+can be measured. Therefore each of these feature variables should each be in 
+it's own row [2]. The multiple columns are converted into row-value pairs using 
+`tidtr::gather()`
+
+8. This detailed tidy data is then groups by subject, activity and feature. 
+The mean of each group is then calculated.
+
+9. In the final step, the tidied, summarised data is written to file.
+
+
+
+### References
+
+[1] Hadley Wickham Tiday, "Tidy Data", Journal of Statistical Software, http://vita.had.co.nz/papers/tidy-data.pdf
+[2] David Hood, Forum Post on Long Data, Wide Data, and Tidy Data for the Assignment https://class.coursera.org/getdata-007/forum/thread?thread_id=214
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,41 @@
+library(dplyr)
+library(plyr)
 
+### ----------------- READ IN SOURCE DATAFILES -------------------------###
 
 # Read in File Listing Features
 features <- read.table ("./UCI HAR Dataset/features.txt", quote="\"")
 
-# Read in the subject performing activity being measured in training set
+# Read in the recorded measurements for training Set
+X_train <- read.table ("./UCI HAR Dataset/train/X_train.txt", quote="\"")
+
+# Read in the file listing the activity being measured for each record in the 
+# training set
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", quote="\"")
+
+# Read in the file listing the subject performing activity being measured in 
+# training set
 subject_train <- read.table ("./UCI HAR Dataset/train/subject_train.txt"
                              , quote="\"")
 
-# Read in Measurements for training Set
-X_train <- read.table ("./UCI HAR Dataset/train/X_train.txt", quote="\"")
+# Read in recorded measurements for test Set
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt", quote="\"")
 
-# Read in activity being measured what is being measured for training set
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", quote="\"")
-   
-# Read in subject performing activity for each measurement in test set
+# Read in the file listing theactivity being measured for each record in the 
+# training set
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", quote="\"")
+
+# Read in the file listing the subject performing activity for each record in 
+# test set
 subject_test <- read.table ("./UCI HAR Dataset/test/subject_test.txt", 
                             quote="\"")
 
-# Read in Measurements for test Set
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt", quote="\"")
 
-# Read in the activity being measured for training set
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", quote="\"")
-
-# Start Step 1 - Merge Training and Test Data
+###-------------------- MERGE TRAINING and TEST DATA -------------------###
 
 # Merge Training and Test Measurement Data
 measurement_data <-rbind(X_train,X_test)
 rm(X_test, X_train)
-
-# Add column description to dataset
-names(measurement_data) <- as.array(features[,2])
 
 # Merge the activity information of the training and test data
 measurement_activity <- rbind(y_train, y_test)
@@ -40,25 +45,27 @@ rm(y_train, y_test)
 measurement_subject <- rbind(subject_train, subject_test)
 rm(subject_train, subject_test)
 
-# End Step 1
+# Provide descriptive variable names to the columns of the measurement_data
+names(measurement_data) <- as.array(features[,2])
 
-# Start Step 2 - Extract only the measurements on mean and standard deviation 
+##------Extract only the measurements on mean and standard deviation ----###
 
-# Identify the columns of the measturements dataset that is related to 
-# standard deviation and mean
+# Firstly need Identify the columns of the measurements dataset that is related 
+# to standard deviation and mean ...
 features_sd_mean_index <- c(grep("mean()",as.array(features[,2]),fixed=TRUE),
                     grep("std()",as.array(features[,2]), fixed=TRUE))
 
-# Extract only the mean and the standard deviation
+rm(features)
+# .. then we use the index above to subset the original measure_data and extract
+# out only the columns that relate to standard deviation and mean.
 
 measurement_sd_mean_data <- measurement_data[,features_sd_mean_index]
 rm(features_sd_mean_index)
+rm(measurement_data)
 
-# End Step 2
+### - Add Descriptive Column that articulates the Activity being measured - ### 
 
-# Start Step 3 and 4 
-
-# Read in Activity Labels
+# Read in Source Activity Labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", 
                               quote="\"")
 
@@ -66,32 +73,63 @@ activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",
 measurement_activity_name <- activity_labels[match(measurement_activity$V1, 
                                                    activity_labels[,1]),2]
 
+rm(activity_labels)
+rm(measurement_activity)
 # Add Activity Names to the sub-setted measurement data
 measurement_sd_mean_data <- cbind(measurement_activity_name , 
                                   measurement_sd_mean_data)
 names(measurement_sd_mean_data)[1] <- "activity"
+rm(measurement_activity_name)
 
-# Add Subject OD to the sub-setted measurement data
+### -------------Add the the subject column to the data -------------------- ### 
+
+# Add Subject ID to the sub-setted measurement data
 measurement_sd_mean_data <- cbind(measurement_subject , 
                                   measurement_sd_mean_data)
 names(measurement_sd_mean_data)[1] <- "subject_id"
 rm(measurement_subject)
 
-# Cleanup feature name to make it more readable
+### -------- Cleanup the variable names to make it more readable ------------###
+
+# Removing all '()'
 names(measurement_sd_mean_data) <- gsub("()","",names(measurement_sd_mean_data),
                                         fixed=TRUE)
+
+# Changing all '-' to '_' as it is more readable
 names(measurement_sd_mean_data) <- gsub("-","_",names(measurement_sd_mean_data),
                                         fixed=TRUE)
 
-# Tidy up the data. Convert the 66 Feature Columns into key value pairs
-tidy_detail_data <- gather(measurement_sd_mean_data,feature_type, feature_value, 
-                           tBodyAcc_mean_X:fBodyBodyGyroJerkMag_std)
+# Fix minor issue in souce data
+names(measurement_sd_mean_data) <- gsub("fBodyBody","fBody",
+                                        names(measurement_sd_mean_data),
+                                        fixed=TRUE)
 
-# Group the tidied up data by the subject, activity and feature
+### ----------------------------- Tidy the Data -----------------------------###
+
+# Tidy up the data. Convert the 66 'Feature' Columns into key value pairs.
+# Treating each of the 66 features as a variable a type of measurement that 
+# therefore needs to be in it's own row.
+
+tidy_detail_data <- gather(measurement_sd_mean_data,feature_type, feature_value, 
+                           tBodyAcc_mean_X:fBodyGyroJerkMag_std)
+
+rm(measurement_sd_mean_data)
+
+### ------------------------Group and Calculate Mean -----------------------###
+
+# Group the tidied up data by the subject, activity and feature so that the 
+# mean can be calculated
 group_tidy_detail_data <- group_by(tidy_detail_data,subject_id,activity,
                                    feature_type)
+rm(tidy_detail_data)
 
 # Calculate the mean of each grouped set of data
-tidy_summarised_data <- summarise(group_tidy_detail_data, mean = mean(feature_value))
+tidy_summarised_data <- summarise(group_tidy_detail_data, 
+                                  feature_mean = mean(feature_value))
 
+rm(group_tidy_detail_data)
 
+### -------------------Write the tidied data to file ------------------------###
+
+write.table(tidy_summarised_data,file = "./output.txt",row.name=FALSE)
+rm(tidy_summarised_data)
